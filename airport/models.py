81313ee3d7997e9513eebc249088a9c43a83b7e7
misha-cw <bigun.mykhaylo@gmail.com>
2026-02-03 17:00:14 +0200

    Add full_name property to Crew model for better name representation

--- airport/models.py
+++ airport/models.py
@@ -34,8 +34,12 @@ class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
+    @property
+    def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
 
 
 class AirplaneType(models.Model):
