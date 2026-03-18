8fe8f5e81ca5fb6979790e1a0a51082e4885c75f
misha-cw <bigun.mykhaylo@gmail.com>
2026-03-18 22:44:33 +0200

    Add tests for User API endpoints

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from user.serializers import UserSerializer
+
+CREATE_USER_URL = reverse("user:create")
+MANAGE_USER_URL = reverse("user:manage")
+
+
+class CreateUserApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user(self):
+        payload = {"email": "user@test.com", "password": "userpassword123"}
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data["email"], payload["email"])
+
+
+class UnauthenticatedManageUserApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(MANAGE_USER_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedManageUserApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="user@test.com", password="userpassword123"
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrive_user(self):
+        res = self.client.get(MANAGE_USER_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        serializer = UserSerializer(self.user)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_put_user(self):
+        payload = {"email": "user2@test.com", "password": "userpassword1234"}
+        res = self.client.put(MANAGE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["email"], payload["email"])
+
+    def test_patch_user(self):
+        payload = {
+            "email": "user2@test.com",
+        }
+        res = self.client.patch(MANAGE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["email"], payload["email"])
