2ae954e60914d4d67068c5a8d3e119f9b7dc64a4
misha-cw <bigun.mykhaylo@gmail.com>
2026-03-31 20:12:05 +0300

    Refactor imports to use absolute paths instead of relative ones for better readability and maintainability

--- airport/admin.py
+++ airport/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import (
+from airport.models import (
     Crew,
     AirplaneType,
     Airplane,
--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .serializers import UserSerializer
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
